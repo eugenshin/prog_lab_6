85c84e2f4e1747ede534ec4971e27ca342c93c8f
Евгений <eugen_shin@mail.ru>
2022-02-22 09:48:17 +0700

    Add lab

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_lab_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            point point1 = new point(), point2 = new point();
+            Console.WriteLine($"Введите координаты первой точки.");
+            point1.vvod();
+            Console.WriteLine($"Введите координаты второй точки.");
+            point2.vvod();
+            Console.WriteLine($"Расстояние между точками = {point1.distance(point2)}");
+            line line1 = new line();
+            Console.WriteLine($"Введите координаты первой и второй точки линии.");
+            line1.vvod();
+            line1.vivod();
+            angle angle1 = new angle();
+            Console.WriteLine($"Введите координаты точкек: на первой стороне угла, вершины, на второй стороне угла.");
+            angle1.vvod();
+            angle1.vivod();
+            Console.WriteLine($"Значение угла = {angle1.value()} градусов.");
+            triangle tri1 = new triangle();
+            Console.WriteLine($"Введите координаты точек треугольника.");
+            tri1.vvod();
+            tri1.vivod();
+            Console.WriteLine($"Площадь треугольника = {tri1.square()}, периметр треугольника = {tri1.per()}");
+            circle circle1 = new circle();
+            Console.WriteLine($"Введите координаты цента круга и его радиус.");
+            circle1.vvod();
+            circle1.vivod();
+            Console.WriteLine($"Площадь круга = {circle1.square()}, периметр круга = {circle1.per()}");
+            segments sm1 = new segments();
+            Console.WriteLine($"Введите координаты цента круга и его радиус.");
+            sm1.vvod();
+            sm1.totalvivod();
+            Console.WriteLine($"Общая длина = {sm1.totallength()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog_lab_6
+{
+    internal class point
+    {
+        private double x, y;
+        public void vvod()
+        {
+            while (!double.TryParse(Console.ReadLine(), out x))
+                Console.WriteLine("Координата x введена неверно.");
+            while (!double.TryParse(Console.ReadLine(), out y))
+                Console.WriteLine("Координата y введена неверно.");
+        }
+        public void vivod()
+        {
+            Console.Write($"x={x}, y={y}");
+        }
+        public double distance(point sdpoint)
+        {
+            return Math.Sqrt(Math.Pow(sdpoint.x - x, 2) + Math.Pow(sdpoint.y - y, 2));
+        }
+        public double getx()
+        {
+            return x;
+        }
+        public double gety()
+        {
+            return y;
+        }
+    }
+}
